5e1f62335d6b1a2bf2d8d13526be0822a7da8b1f
GraceD15 <graceful.deng@gmail.com>
2025-04-15 02:38:06 +0000

    create kafka consumer

--- instalite/server/kafka/consumer.js
+++ instalite/server/kafka/consumer.js
@@ -1,22 +1,22 @@
 ///////////////
 // Adapted from NETS 2120 Sample Kafka Client
 ///////////////
-
-import express from 'express';
+import axios from 'axios';
 import pkg from 'kafkajs';
-const { Kafka, CompressionTypes, CompressionCodecs } = pkg;
 import SnappyCodec from 'kafkajs-snappy';
+import fs from 'fs';
+import dotenv from 'dotenv';
 
+// Set-up
+const { Kafka, CompressionTypes, CompressionCodecs } = pkg;
 // Add snappy codec to the CompressionCodecs.
 CompressionCodecs[CompressionTypes.Snappy] = SnappyCodec;
-
-import fs from 'fs';
 const configFile = fs.readFileSync('server/kafka/config.json', 'utf8');
-import dotenv from 'dotenv';
 dotenv.config();
 const config = JSON.parse(configFile);
 console.log(`Config: ${JSON.stringify(config)}`);
-const app = express();
+
+// Open connection to Kafka
 const kafka = new Kafka({
     clientId: 'my-app',
     brokers: config.bootstrapServers
@@ -24,35 +24,46 @@ const kafka = new Kafka({
 
 const consumer = kafka.consumer({ 
     groupId: config.groupId, 
-    bootstrapServers: config.bootstrapServers});
-
-var kafka_messages = [];
-
-app.get('/', (req, res) => {
-    res.send(JSON.stringify(kafka_messages));
+    bootstrapServers: config.bootstrapServers
 });
+
+var bluesky_posts = [];
+var federated_posts = [];
 
 const run = async () => {
     // Consuming
     await consumer.connect();
-    console.log(`Following topic ${config.topic}`);
-    await consumer.subscribe({ topic: config.topic, fromBeginning: true,
-        compression: CompressionTypes.Snappy });
+    await producer.connect();
+
+    // Subscribing
+    console.log("Following topic Bluesky-Kafka");
+    await consumer.subscribe({ topic: 'Bluesky-Kafka', fromBeginning: true, compression: CompressionTypes.Snappy });
+    
+    console.log("Following topic FederatedPosts");
+    await consumer.subscribe({ topic: 'FederatedPosts', fromBeginning: true, compression: CompressionTypes.Snappy });
 
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
-            kafka_messages.push({
-                value: message.value.toString(),
-            });
-            console.log({
-                value: message.value.toString(),
-            });
+            if (topic === 'Bluesky-Kafka') {
+                bluesky_posts.push({
+                    value: message.value.toString(),
+                });
+                console.log('Bluesky post');
+            } else if (topic === 'FederatedPosts') {
+                federated_posts.push({
+                    value: message.value.toString(),
+                });
+                console.log(`Federated post: ${message.value.toString()}`);
+            } else {
+                console.log(`Unknown topic: ${topic}`);
+            }
+
+            // Upload post to app
+            // await axios.post(`${config.serverRootURL}/federatedPost`, {
+                
+            // });
         },
     });
 };
 
 run().catch(console.error);
-
-app.listen(config.port, () => {
-    console.log(`App is listening on port ${config.port} -- you can GET the Kafka messages`);
-});
