21d05572fa55ae819456d2f442506db999cc2bab
AC2005 <changandrew777@gmail.com>
2025-05-09 13:32:21 +0000

    remove more imports

--- frontend/src/pages/Friends.tsx
+++ frontend/src/pages/Friends.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import config from '../../config.json';
 import { useNavigate } from 'react-router-dom';
 import { getSocket } from '../Socket';
 import Sidebar from '../components/Sidebar';
